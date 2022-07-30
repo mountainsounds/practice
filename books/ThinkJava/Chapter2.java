12219065675152ace022f61484c852cc9a685ed8
Zachary Sawyer <zsaw@Zacharys-iMac.local>
2022-07-30 17:28:40 -0400

    progress

---
+++ books/ThinkJava/Chapter2.java
@@ -0,0 +1,12 @@
+package books.ThinkJava;
+
+public class Chapter2 {
+    public static void main (String[] args) {
+        String firstName = "Zack";
+        String lastName = "Sawyer";
+        int age = 29;
+        
+        System.out.println("Hello, " + firstName + " " + lastName);
+        System.out.println("You are " + age + " years old");
+    }
+}
